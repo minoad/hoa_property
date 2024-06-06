250beee52a88a2e6093ddb9153a057b629b9db97
Micah Norman <minoad@gmail.com>
2024-06-06 14:08:56 -0500

    added exception testing for pdf extraction

--- src/extract/pdf.py
+++ src/extract/pdf.py
@@ -12,10 +12,34 @@ from hoa_property import logger
 @dataclass()
 class PDFFile:
     """
-    Represents a single pdf file.
+    A class to represent a directory of PDF files.
+
+    This class provides methods to collect PDF files from a specified filepath or directory.
+    It checks if the path is a directory or a single PDF file. If it's a directory, it collects
+    all PDF files within the directory. If it's a single PDF file, it collects the file.
+    If the path is neither a directory nor a PDF file, it logs an error.
+    If a FileNotFoundError occurs, it logs the error and returns None.
+
+    Attributes:
+        path (str): The path to the directory or file.
+
+    Methods:
+        get_pdf_files(): Collects PDF files from the specified filepath or directory.
     """
 
     filepath: str
+
+
+class PDFDirectoryGeneralException(Exception):
+    """
+    Exception raised for general errors in the PDFDirectory class.
+
+    This class is a custom exception class that can be used to raise exceptions for
+    non-specific errors that occur within the PDFDirectory class.
+
+    Attributes:
+       message (str): An optional message that provides more details about the error.
+    """
 
 
 @dataclass()
@@ -26,17 +50,25 @@ class PDFDirectory:
 
     path: str
 
-    def get_pdf_files(self) -> list[PDFFile] | None:
+    def get_pdf_files(self) -> list[PDFFile]:
         """
-        Collect pdf files from a filepath or a directory.
+        Collects PDF files from a specified filepath or directory.
+
+        This method checks if the path is a directory or a single PDF file.
+            If it's a directory, it collects all PDF files within the directory.
+            If it's a single PDF file, it collects the file.
+            If the path is neither a directory nor a PDF file, it logs an error.
+            If a FileNotFoundError occurs, it logs the error and returns None.
+
+        Returns:
+            list[PDFFile] | None: A list of PDFFile objects if PDF files are found, None otherwise.
         """
         pdf_files: list[PDFFile] = []  # Make mypy stop complaining
         try:
             p = Path(self.path)
             if p.is_dir():
-                pdf_files = [
-                    PDFFile(filepath=str(object=file)) for file in p.iterdir() if file.suffix.lower() == ".pdf"
-                ]
+                pdf_files = [PDFFile(filepath=str(file)) for file in p.iterdir() if file.suffix.lower() == ".pdf"]
+                logger.debug("checking path: %s, found %s", self.path, pdf_files)
                 return pdf_files
             if p.is_file() and p.suffix.lower() == ".pdf":
                 pdf_files = [PDFFile(filepath=str(object=p))]
@@ -44,7 +76,9 @@ class PDFDirectory:
             logger.error("The path %s is neither a directory nor a PDF file.", self.path)
         except FileNotFoundError as e:
             logger.error("An error occurred while retrieving PDF files: %s", e)
-        return None
+            raise e
+
+        raise PDFDirectoryGeneralException(f"An error occurred while retrieving PDF files fro {self.path}.")
 
     def __post_init__(self):
 
--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -12,31 +12,58 @@ pdf_files = PDFDirectory(TEST_PDF_PATH)
 
 
 test_cases: dict[str, dict[str, Any]] = {
-    "multi_pdfs_in_path": {
+    "invalid_path": {
         "function": PDFDirectory,
-        "input": {"path": "data/test/plats"},
-        "expected": PDFDirectory(path="data/test/plats"),
-        "expected_to_fail": False,
+        "input": {"path": "invalid/path"},
+        "expected": None,
+        "expected_to_fail": True,
     },
-    "single_pdfs_in_path": {
-        "function": PDFDirectory,
-        "input": {"path": "data/test/plats/Cap Rock 1 Recorded Plat.pdf"},
-        "expected": PDFDirectory(path="data/test/plats/Cap Rock 1 Recorded Plat.pdf"),
-        "expected_to_fail": False,
-    },
+    # "multi_pdfs_in_path": {
+    #     "function": PDFDirectory,
+    #     "input": {"path": "data/test/plats"},
+    #     "expected": PDFDirectory(path="data/test/plats"),
+    #     "expected_to_fail": False,
+    # },
+    # "single_pdfs_in_path": {
+    #     "function": PDFDirectory,
+    #     "input": {"path": "data/test/plats/Cap Rock 1 Recorded Plat.pdf"},
+    #     "expected": PDFDirectory(path="data/test/plats/Cap Rock 1 Recorded Plat.pdf"),
+    #     "expected_to_fail": False,
+    # },
 }
 
 
 @pytest.mark.parametrize("name, case", test_cases.items())
 def test_pdfs(name, case):
+    """
+    Tests the PDF processing functions with various test cases.
+
+    This function is parameterized with a dictionary of test cases. Each test case is a dictionary
+    that includes the function to be tested, the input for the function, the expected result, and
+    a flag indicating whether the test case is expected to fail.
+
+    If the test case is expected to fail, the function checks that an exception is raised.
+    Otherwise, it checks that the function returns the expected result.
+
+    Args:
+        name (str): The name of the test case.
+        case (dict): The test case. It should have the following keys:
+            - "function": The function to be tested.
+            - "input": The input for the function.
+            - "expected": The expected result.
+            - "expected_to_fail": A flag indicating whether the test case is expected to fail.
+
+    Raises:
+        Exception: If the test case is expected to fail and no exception is raised.
+        AssertionError: If the function does not return the expected result.
+    """
     func = case["function"]
     inputs = case["input"]
-    result = func(**inputs)
 
     # Handle cases where input might be missing or incorrect
     if case["expected_to_fail"]:
         with pytest.raises(Exception):
-            func(**inputs)
+            result = func(**inputs)
     else:
         result = func(**inputs)
         assert result == case["expected"], f"Test {name} failed: expected {case['expected']} but got {result}"
